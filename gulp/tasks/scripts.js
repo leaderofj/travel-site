f2d65047e22c2d08f62b44b9d0a902de5f035843
Yun PC <mycadworld@gmail.com>
2018-01-03 21:14:07 +0900

    Completed desktop and mobile header styles.

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    webpack = require('webpack');
+
+gulp.task('scripts', function (callback) {
+    /*웹팩은 err 과 stats */
+    webpack(require('../../webpack.config'), function (err,stats) {
+        if(err) {
+            console.log(err.toString());
+        }
+        console.log(stats.toString());
+        callback();
+    });
+});
